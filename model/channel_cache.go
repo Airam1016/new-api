fbf9bb468a4741da5da02dfd1b03981600144ac1
Fanu <fangxu@foxmail.com>
2026-05-22 17:27:19 +0800

    fix: import syntax error in model/channel_cache.go

--- model/channel_cache.go
+++ model/channel_cache.go
@@ -12,8 +12,9 @@ import (
 
 	"github.com/QuantumNous/new-api/common"
 	"github.com/QuantumNous/new-api/constant"
-"github.com/QuantumNous/new-api/dto"
-"github.com/QuantumNous/new-api/logger"	"github.com/QuantumNous/new-api/setting/ratio_setting"
+	"github.com/QuantumNous/new-api/dto"
+	"github.com/QuantumNous/new-api/logger"
+	"github.com/QuantumNous/new-api/setting/ratio_setting"
 )
 
 var group2model2channels map[string]map[string][]int // enabled channel
